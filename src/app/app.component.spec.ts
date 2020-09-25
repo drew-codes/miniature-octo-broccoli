86baf0b7ea649a7087a1f752e8d52d10dab2c288
Drew <atthompson92@gmail.com>
2020-09-25 13:11:15 -0400

    add tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AnalogClockComponent } from './analog-clock/analog-clock.component';
 import { AppComponent } from './app.component';
+import { DigitalClockComponent } from './digital-clock/digital-clock.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let analogClock: AnalogClockComponent;
+  let digitalClock: DigitalClockComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AnalogClockComponent,
+        DigitalClockComponent
       ],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    const analogClockQuery = fixture.debugElement.query(By.directive(AnalogClockComponent));
+    const digitalClockQuery = fixture.debugElement.query(By.directive(DigitalClockComponent));
+    analogClock = analogClockQuery.componentInstance;
+    digitalClock = digitalClockQuery.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'synced-clocks'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('synced-clocks');
+  it('should render the analog clock', () => {
+    expect(analogClock).toBeTruthy();
+  });
+  
+  it('should render the digital clock', () => {
+    expect(digitalClock).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should update the time if a onTimeChange event is triggered on the child digital clock', () => {
+    digitalClock.onTimeChange.emit('12:30:30');
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('synced-clocks app is running!');
+    expect(app.time).toBe('12:30:30');
+  });
+  
+  it('should update the time if a onTimeChange event is triggered on the child analog clock', () => {
+    analogClock.onTimeChange.emit('12:30:30');
+    fixture.detectChanges();
+    expect(app.time).toBe('12:30:30');
   });
 });
--- src/app/digital-clock/digital-clock.component.spec.ts
+++ src/app/digital-clock/digital-clock.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { TimeInputComponent } from '../time-input/time-input.component';
 
 import { DigitalClockComponent } from './digital-clock.component';
 
 describe('DigitalClockComponent', () => {
   let component: DigitalClockComponent;
   let fixture: ComponentFixture<DigitalClockComponent>;
+  let timeInput: TimeInputComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DigitalClockComponent ]
+      declarations: [ DigitalClockComponent, TimeInputComponent ]
     })
     .compileComponents();
   });
@@ -16,10 +19,34 @@ describe('DigitalClockComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DigitalClockComponent);
     component = fixture.componentInstance;
+    const timeInputQuery = fixture.debugElement.query(By.directive(TimeInputComponent));
+    timeInput = timeInputQuery.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the digital clock', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create the time input', () => {
+    expect(timeInput).toBeTruthy();
+  })
+
+  it('should accept a time 24-hour time input and set the time accordingly in 12-hour time in the DOM', () => {
+    component.time = '16:45:30';
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const paragraph = compiled.querySelector('p').textContent;
+    expect(paragraph).toBe('04:45:30 pm');
+  });
+
+  it('should emit an onTimeChange event when the child onTimeChange event is fired', () => {
+    spyOn(component.onTimeChange, 'emit');
+    timeInput.onTimeChange.emit('12:30:30');
+    fixture.detectChanges();
+    expect(component.onTimeChange.emit).toHaveBeenCalledWith('12:30:30');
+    expect(component.onTimeChange.emit).toHaveBeenCalledTimes(1);
+    expect(component.onTimeChange.emit).not.toHaveBeenCalledTimes(2);
+  });
+
 });
